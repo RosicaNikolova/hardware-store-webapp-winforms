7adcb4a360fc3d1cf7795f79b43cfe69dd6a9e6e
Dandi <476545@student.fontys.nl>
2022-02-24 09:51:07 +0100

    Commiting base project forms

--- MediaBazaarProject/MediaBazaarProject/Form1.Designer.cs
+++ MediaBazaarProject/MediaBazaarProject/Form1.Designer.cs
@@ -29,13 +29,78 @@ namespace MediaBazaarProject
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.tbEmailLogin = new System.Windows.Forms.TextBox();
+            this.tbPasswordLogin = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(147, 134);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(46, 20);
+            this.lblEmail.TabIndex = 0;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(147, 216);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(70, 20);
+            this.lblPassword.TabIndex = 1;
+            this.lblPassword.Text = "Password";
+            // 
+            // tbEmailLogin
+            // 
+            this.tbEmailLogin.Location = new System.Drawing.Point(147, 157);
+            this.tbEmailLogin.Name = "tbEmailLogin";
+            this.tbEmailLogin.Size = new System.Drawing.Size(125, 27);
+            this.tbEmailLogin.TabIndex = 2;
+            // 
+            // tbPasswordLogin
+            // 
+            this.tbPasswordLogin.Location = new System.Drawing.Point(147, 239);
+            this.tbPasswordLogin.Name = "tbPasswordLogin";
+            this.tbPasswordLogin.Size = new System.Drawing.Size(125, 27);
+            this.tbPasswordLogin.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(160, 292);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(94, 29);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(457, 525);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.tbPasswordLogin);
+            this.Controls.Add(this.tbEmailLogin);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblEmail);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox tbEmailLogin;
+        private System.Windows.Forms.TextBox tbPasswordLogin;
+        private System.Windows.Forms.Button btnLogin;
     }
 }
 
