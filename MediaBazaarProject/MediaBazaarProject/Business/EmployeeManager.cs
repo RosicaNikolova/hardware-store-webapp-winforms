6769e086ea01531c7c6174c9e07f4c5aae816def
Jakub Jelinek <i476736@fhict.nl>
2022-03-09 11:44:59 +0100

    created structure(Presentation, Business, Persistence); added class EmployeeManager

---
+++ MediaBazaarProject/MediaBazaarProject/Business/EmployeeManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBazaarProject.Business
+{
+    public class EmployeeManager
+    {
+        /*EmployeeRepository employeeRepository = new EmployeeRepository();
+        public Employee CreateEmployee(string email, string password) {
+            
+        }
+        public void UpdateEmployee(Employee e) {
+            
+        }
+        public void DeactivateEmployee(Employee e)
+        {
+
+        }
+        public void DeleteEmployee(Employee e) {
+            
+        }*/
+    }
+}
