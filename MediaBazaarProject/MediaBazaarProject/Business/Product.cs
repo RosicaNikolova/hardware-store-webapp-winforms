fa497ff4edb929230518630d7367c5fd2c61a15c
Dandi <476545@student.fontys.nl>
2022-03-31 11:33:21 +0200

    Uploading product class

---
+++ MediaBazaarProject/MediaBazaarProject/Business/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBazaarProject.Business
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string ProductDescription { get; set; }
+
+        public string ProductManufacturer { get; set; }
+
+        public string QuantityWarehouse { get; set; }
+
+        public string QuantitySales { get; set; }
+
+        public string ProductCategory { get; set; }
+    }
+}
