468ac7b791fc1574c961dad2ff03dbd4d7318db1
Dandi <476545@student.fontys.nl>
2022-03-31 11:49:37 +0200

    Uploading ProductRepository

---
+++ MediaBazaarProject/MediaBazaarProject/Persistence/ProductRepository.cs
@@ -0,0 +1,37 @@
+﻿using MediaBazaarProject.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBazaarProject.Persistence
+{
+    public class ProductRepository
+    {
+        public List<Product> GetAllProducts()
+        {
+
+        }
+
+        public List<Product> GetProductsByCategory()
+        {
+
+        }
+
+        public Product GetProduct(int id)
+        {
+
+        }
+
+        public bool UpdateProduct(Product product)
+        {
+
+        }
+
+        public bool DeleteProduct(Product product)
+        {
+
+        }
+    }
+}
