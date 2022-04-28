90f6e616d1083e69a12fa301b8c4c75886715496
Jakub Jelinek <i476736@fhict.nl>
2022-04-29 01:21:32 +0200

    website changes

--- MediaBazaarProject/MediaBazaarWebsite/Pages/LeaveRequestPage.cshtml.cs
+++ MediaBazaarProject/MediaBazaarWebsite/Pages/LeaveRequestPage.cshtml.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MediaBazaarLibrary.Business;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MediaBazaarWebsite.Pages
 {
+    [Authorize]
     public class LeaveRequestPageModel : PageModel
     {
         [BindProperty]
